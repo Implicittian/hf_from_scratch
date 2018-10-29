d59efc1239d908e22520fe141836421d60aa6114
Implicittian <33207576+Implicittian@users.noreply.github.com>
2018-10-29 15:44:36 +0000

    Add files via upload

--- atom.py
+++ atom.py
@@ -5,17 +5,26 @@ Created on Sun Oct 28 14:44:02 2018
 @author: Acer
 """
 from parseData import parseData;
-
+from integral import orbitalIntegral;
+import numpy as np;
+"""
+Class Atom Data Structure:
+    charge = nuclear charge
+    orbital is a list of tuples in the form of 
+    [('S', [(zeta1, coeff1), (zeta2, coeff2), ...])]
+    from which we see that the first 
+"""
 class Atom:
     def __init__(self, basis, pos):
-        (self.charge, self.orbital) = basis;     
+        (self.charge, self.orbitals) = basis;     
         self.pos = pos;
+        self._normOrbital();
     
     def getOrbital(self, index=0):
-        return self.orbital[index][1];
+        return self.orbitals[index][1];
     
     def getOrbitalType(self, index=0):
-        return self.orbitalType[index][0];
+        return self.orbitals[index][0];
     
     def getAllZeta(self, index=0):
         orbital = self.getOrbital(index);
@@ -50,18 +59,65 @@ class Atom:
     def getPos(self):
         return self.pos;
     
+    def setX(self, newX):
+        self.pos[0] = newX;
+        
+    def setY(self, newY):
+        self.pos[1] = newY;
+        
+    def setZ(self, newZ):
+        self.pos[2] = newZ;
+        
+    def setPos(self, pos):
+        (x, y, z) = pos;
+        self.setX(x);
+        self.setY(y);
+        self.setZ(z);
+    
+    def _getOrbitalModulus(self):
+        allOrbitalModulus = [];
+        for orbitalIndex in range(len(self.orbitals)):
+            orbital = self.getOrbital(orbitalIndex);
+            orbitalModulus = 0;
+            for i in range(len(orbital)):
+                for j in range(len(orbital)):
+                    orbitalModulus = orbitalModulus + \
+                                     orbitalIntegral(orbital[i], orbital[j], self.pos, self.pos);
+            allOrbitalModulus.append(np.sqrt(orbitalModulus));
+        return allOrbitalModulus;
+    
+    def _normOrbital(self):
+        allOrbitalModulus = self._getOrbitalModulus();
+        for orbitalIndex in range(len(self.orbitals)):
+            orbitalModulus = allOrbitalModulus[orbitalIndex];
+            orbital = self.getOrbital(orbitalIndex);
+            for coeffIndex in range(len(orbital)):
+                (zeta, coeff) = orbital[coeffIndex];
+                orbital[coeffIndex] = (zeta, coeff/orbitalModulus);
+                
+    def printAtom(self):
+        print("The charge is " + str(self.charge) + "\n");
+        print("There are " + str(len(self.orbitals)) + " orbitals \n");
+        for i in range(len(self.orbitals)):
+            orbital = self.getOrbital(i);
+            print("The type of orbital is " + self.getOrbitalType(i));
+            print("It has " + str(len(orbital)) + " contracted component Gaussians");
+            for (zeta, coeff) in orbital:
+                print("The zeta of component orbital is " + str(zeta));
+                print("The coefficient of component orbital is " + str(coeff));
+    
 def buildAtomListSTO3G(basisFile, allData):
     """
     allData will be in the form of a list, where each element is given as
     a tuple (AtomType, AtomCoord)
-    E.g. ('H', (0,0,1))
+    E.g. ('H',0,0,1)
     """
     atomList = [];
     allBasis = parseData(basisFile, "STO-3G");
     for i in range(len(allData)):
-        (AtomType, AtomPos) = allData[i];
+        (AtomType, posX, posY, posZ) = allData[i];
         basis = allBasis[AtomType];
-        atom = Atom(basis, AtomPos);
+        atom = Atom(basis, (posX, posY, posZ));
         atomList.append(atom);
     return atomList;
             
--- integral.py
+++ integral.py
@@ -7,26 +7,37 @@ Created on Sun Oct 28 13:43:02 2018
 
 import numpy as np;
 
-def gaussProd(zetaTuple1, zetaTuple2, centre1, centre2):
-    """
-    This function takes four input
-    (zeta1, coeff1), centre1 specifies coeff1*exp(-zeta(x-centre1))
-    (zeta2, coeff2), centre2 specifies coeff2*exp(-zeta(x-centre2))
-    
-    This function returns
-    coeff1*coeff2*exp(-zeta1*zeta2/(zeta1+zeta2)*(centre2-centre1)^2)
-    *exp(-(zeta1+zeta2)*(x-Px))
-    Px = zeta1*centre1+zeta2*centre2/(zeta1+zeta2)
-    represented as (newZeta, newCoeff), newCentre 
-    """
-    (zeta1, coeff1) = zetaTuple1;
-    (zeta2, coeff2) = zetaTuple2;
-    newZeta = zeta1 + zeta2;
-    newCoeff = coeff1*coeff2*np.exp(-zeta1*zeta2/(zeta1+zeta2)*(centre1-centre2)**2);
-    newCentre = zeta1*centre1 + zeta2*centre2/ (zeta1 + zeta2);
-    return (newZeta, newCoeff), newCentre;
-
-def getNorm(zetaList):
-    for i in range(len(zetaList)):
-        for j in range(len(zetaList)):
-            
+# =============================================================================
+# def gaussProd(zetaTuple1, zetaTuple2, centre1, centre2):
+#     """
+#     This function takes four input
+#     (zeta1, coeff1), centre1 specifies coeff1*exp(-zeta(x-centre1))
+#     (zeta2, coeff2), centre2 specifies coeff2*exp(-zeta(x-centre2))
+#     
+#     This function returns
+#     coeff1*coeff2*exp(-zeta1*zeta2/(zeta1+zeta2)*(centre2-centre1)^2)
+#     *exp(-(zeta1+zeta2)*(x-Px))
+#     Px = zeta1*centre1+zeta2*centre2/(zeta1+zeta2)
+#     represented as (newZeta, newCoeff), newCentre 
+#     """
+#     (zeta1, coeff1) = zetaTuple1;
+#     (zeta2, coeff2) = zetaTuple2;
+#     newZeta = zeta1 + zeta2;
+#     newCoeff = coeff1*coeff2*np.exp(-zeta1*zeta2/(zeta1+zeta2)*(centre1-centre2)**2);
+#     newCentre = zeta1*centre1 + zeta2*centre2/ (zeta1 + zeta2);
+#     return (newZeta, newCoeff), newCentre;
+# =============================================================================
+def orbitalIntegral(orbital1, orbital2, centre1, centre2):
+    (zeta1, coeff1) = orbital1;
+    (zeta2, coeff2) = orbital2;
+    coordinate1 = np.array(centre1);
+    coordinate2 = np.array(centre2);
+    QDistSquared = (coordinate1 - coordinate2).dot((coordinate1 - coordinate2));
+    q = zeta1*zeta2/(zeta1 + zeta2);
+    p = zeta1 + zeta2;
+    return coeff1*coeff2*np.exp(-q*QDistSquared)*(np.pi/p)**(3/2);
+# =============================================================================
+# 
+# def overlapMatrix(orbitals1, orbitals2, centre1, centre2):
+#     
+# =============================================================================
